55619488cf6e29dfdf318ac986afafc4147038da
grabania <grabania.work@gmail.com>
2020-05-23 00:03:30 +0100

    Add destructuring arrays quiz

--- ES6.js
+++ ES6.js
@@ -133,3 +133,33 @@ const something = {
 const { brand, location, size } = something;
 
 console.log(brand, location, size);
+
+/*
+ * Programming Quiz: Destructuring Arrays
+ *
+ * Use destructuring to initialize the variables `one`, `two`, and `three`
+ * with the colors from the `things` array.
+ */
+// const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'mars', 'blue', 'dogs'];
+
+// const one = things;
+// const two = '';
+// const three = '';
+
+// const colors = `List of Colors
+// 1. ${one}
+// 2. ${two}
+// 3. ${three}`;
+
+// console.log(colors);
+
+const things = [ 'red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'mars', 'blue', 'dogs' ];
+
+const [ one, , , two, , , , three ] = things;
+
+const colors = `List of Colors
+1. ${one}
+2. ${two}
+3. ${three}`;
+
+console.log(colors);
