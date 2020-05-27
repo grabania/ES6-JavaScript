6400e00997f4dd81522a321f44ddfbec810816dc
grabania <grabania.work@gmail.com>
2020-05-27 23:55:36 +0100

    Add Spread Operator Examples

--- ES6.js
+++ ES6.js
@@ -261,3 +261,11 @@ for (const day of days) {
 	var upperDay = day.charAt(0).toUpperCase() + day.slice(1);
 	console.log(upperDay);
 }
+
+//Spread operator(ES6)
+
+const books = [ 'Don Quixote', 'The Hobbit', 'Alice in Wonderland', 'Tale of Two Cities' ];
+console.log(...books);
+
+const primes = new Set([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
+console.log(...primes);
